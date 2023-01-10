8daa6d70fa74cfe26d63b778cfed34604774bd89
Qi <qi-001@outlook.com>
2023-01-10 13:05:58 +0800

    1-10 2023 pretrained --> release

---
+++ scripts/download_pretrained.sh
@@ -0,0 +1,5 @@
+wget https://github.com/bon-qi/nerf/releases/download/1.0/chair_020000.tar
+wget https://github.com/bon-qi/nerf/releases/download/1.0/fern_020000.tar
+wget https://github.com/bon-qi/nerf/releases/download/1.0/ficus_020000.tar
+wget https://github.com/bon-qi/nerf/releases/download/1.0/lego_020000.tar
+mv chair_020000.tar fern_020000.tar ficus_020000.tar lego_020000.tar ./pretrained
